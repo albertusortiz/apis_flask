172ed8a8ac15d024cc76225383b0958039216f4f
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2021-08-01 00:02:16 -0500

    Mostrando tareas.

--- api/app/views.py
+++ api/app/views.py
@@ -1,18 +1,24 @@
 from flask import Blueprint
 
+from .models.task import Task
 from .responses import response
 
 api_v1 = Blueprint('api', __name__, url_prefix='/api/v1')
 
 @api_v1.route('/tasks', methods=['GET'])
 def get_tasks():
-    return response({
-        'mensaje': 'Nuevo mensaje'
-    })
+
+    tasks = Task.query.all()
+
+    return response([
+        task.serialize() for task in tasks
+    ])
 
 @api_v1.route('/tasks/<id>', methods=['GET'])
-def get_task():
-    pass
+def get_task(id):
+    task = Task.query.filter_by(id=id).first()
+
+    return response(task.serialize())
 
 @api_v1.route('/tasks', methods=['POST'])
 def create_task():
