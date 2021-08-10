afab417d48c4be7cd143d8724d7f9bd47fef4a39
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2021-08-09 21:39:23 -0500

    Serializando objetos.

--- api/app/views.py
+++ api/app/views.py
@@ -6,7 +6,24 @@ from .responses import response
 from .responses import not_found
 from .responses import bad_request
 
+from .schemas import task_schema
+from .schemas import tasks_schema
+
 api_v1 = Blueprint('api', __name__, url_prefix='/api/v1')
+
+def set_task(function):
+    def wrap(*args, **kwargs):
+        id = kwargs.get('id', 0)
+        task = Task.query.filter_by(id=id).first()
+
+        if task is None:
+            return not_found()
+
+        print("Entramos al decorador.")
+        return function(task)
+
+    wrap.__name__ = function.__name__
+    return wrap
 
 @api_v1.route('/tasks', methods=['GET'])
 def get_tasks():
@@ -15,18 +32,12 @@ def get_tasks():
 
     tasks = Task.get_by_page(order, page)
 
-    return response([
-        task.serialize() for task in tasks
-    ])
+    return response(tasks_schema.dump(tasks))
 
 @api_v1.route('/tasks/<id>', methods=['GET'])
-def get_task(id):
-    task = Task.query.filter_by(id=id).first()
-
-    if task is None:
-        return not_found()
-
-    return response(task.serialize())
+@set_task
+def get_task(task):
+    return response(task_schema.dump(task))
 
 @api_v1.route('/tasks', methods=['POST'])
 def create_task():
@@ -45,17 +56,13 @@ def create_task():
     task = Task.new(json['title'], json['description'], json['deadline'])
 
     if task.save():
-        return response(task.serialize())
+        return response(task_schema.dump(task))
 
     return bad_request()
 
 @api_v1.route('/tasks/<id>', methods=['PUT'])
-def update_task(id):
-    task = Task.query.filter_by(id=id).first()
-
-    if task is None:
-        return not_found()
-
+@set_task
+def update_task(task):
     json = request.get_json(force=True)
 
     task.title = json.get('title', task.title)
@@ -63,18 +70,14 @@ def update_task(id):
     task.deadline = json.get('deadline', task.deadline)
 
     if task.save():
-        return response(task.serialize())
+        return response(task_schema.dump(task))
     
     return bad_request()
 
 @api_v1.route('/tasks/<id>', methods=['DELETE'])
-def delete_task(id):
-    task = Task.query.filter_by(id=id).first()
-
-    if task is None:
-        return not_found()
-
+@set_task
+def delete_task(task):
     if task.delete():
-        return response(task.serialize())
+        return response(task_schema.dump(task))
 
     return bad_request()
